431b19913ea24be760a32066bf96bca803507364
Jaymeson Mendes <jaymesonmendes@gmail.com>
2023-02-19 23:44:32 -0300

    feature: função de getAll e getById que n estv fun

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,30 +1,27 @@
-const db = require("../database/db.js");
+const { db, getAllUsers, getUserById } = require("../database/db.js");
 const Joi = require("joi");
 
 const userController = {
   async getAllUsers(ctx) {
-    const sql = "SELECT * FROM users";
     try {
-      const rows = await db.all(sql);
-      const total = rows.length;
-      ctx.body = { total, rows };
-      console.log(rows);
+      const users = await getAllUsers();
+      ctx.response.status = 200;
+      ctx.body = { users };
     } catch (err) {
-      ctx.status = 500;
-      ctx.body = { message: err.message };
+      ctx.response.status = 500;
+      ctx.body = { error: err.message };
     }
   },
 
   async getUserById(ctx) {
     const { id } = ctx.params;
-    const sql = "SELECT * FROM users WHERE id = ?";
     try {
-      const row = await db.get(sql, id);
-      if (!row) {
+      const user = await getUserById(id);
+      if (!user) {
         ctx.status = 404;
         ctx.body = { message: `Usuário com id ${id} não encontrado.` };
       } else {
-        ctx.body = row;
+        ctx.body = user;
       }
     } catch (err) {
       ctx.status = 500;
@@ -32,43 +29,44 @@ const userController = {
     }
   },
 
- async createUser(ctx) {
-  const { name, email, password } = ctx.request.body || {};
-  
-  // Definir o schema de validação
-  const schema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required()
-  });
+  async createUser(ctx) {
+    const { name, email, password, img } = ctx.request.body || {};
 
-  // Validar os dados da requisição com base no schema
-  const { error, value } = schema.validate({ name, email, password });
-  if (error) {
-    ctx.status = 400;
-    ctx.body = { message: error.details[0].message };
-    return;
-  }
+    // Definir o schema de validação
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      img: Joi.string().required(),
+    });
 
-  // Inserir os dados no banco de dados
-  const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
-  try {
-    const result = await db.run(sql, value.name, value.email, value.password);
-    ctx.status = 201;
-    ctx.body = { id: result.lastID };
-  } catch (err) {
-    ctx.status = 400;
-    ctx.body = { message: err.message };
-  }
-},
+    // Validar os dados da requisição com base no schema
+    const { error, value } = schema.validate({ name, email, password, img });
+    if (error) {
+      ctx.status = 400;
+      ctx.body = { message: error.details[0].message };
+      return;
+    }
+
+    // Inserir os dados no banco de dados
+    const sql = "INSERT INTO users (name, email, password, img) VALUES (?, ?, ?, ?)";
+    try {
+      const result = await db.run(sql, value.name, value.email, value.password, value.img);
+      ctx.status = 201;
+      ctx.body = { id: result.lastID };
+    } catch (err) {
+      ctx.status = 400;
+      ctx.body = { message: err.message };
+    }
+  },
 
   async updateUser(ctx) {
     const { id } = ctx.params;
-    const { name, email, password } = ctx.request.body || {};
+    const { name, email, password, img } = ctx.request.body || {};
     const sql =
-      "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?";
+      "UPDATE users SET name = ?, email = ?, password = ?, img = ? WHERE id = ?";
     try {
-      const result = await db.run(sql, name, email, password, id);
+      const result = await db.run(sql, name, email, password, img, id);
       if (result.changes === 0) {
         ctx.status = 404;
         ctx.body = { message: `Usuário com id ${id} não encontrado.` };
@@ -91,7 +89,8 @@ const userController = {
         ctx.status = 404;
         ctx.body = { message: `Usuário com id ${id} não encontrado.` };
       } else {
-        ctx.status = 204;
+        ctx.status = 200;
+        ctx.body = { message: `Usuário com id ${id} excluído.` };
       }
     } catch (err) {
       ctx.status = 500;
